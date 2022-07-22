3779e811ca19ad9a8c428032c06c42901f3739cb
Marcelo Henrique Leite da Silva <mhsilva@leega.local>
2022-07-22 15:36:58 -0300

    criando arquvio variables e alterado o arquimo main

---
+++ iac/variables.tf
@@ -0,0 +1,16 @@
+variable "bucket_name" {
+    default = "datalake"
+}
+
+variable "environment" {
+    default = "production"
+}
+
+variable "account_number" {
+    default = "123"  
+}
+
+
+variable "region" {
+  default = "us-east-1"
+}
